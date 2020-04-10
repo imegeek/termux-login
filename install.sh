83fd60dc0662ba885b4afb5009234f4250ebafbf
abhackerofficial <63346676+abhackerofficial@users.noreply.github.com>
2020-04-10 10:50:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-apt install figlet  
+
 apt install ruby 
 gem install lolcat
+pkg install toilet
 apt install python
 apt install python2 
 pip install requests
 pip2 install requests
-pkg install mpv
+
